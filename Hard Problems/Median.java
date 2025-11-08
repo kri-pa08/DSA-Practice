536211faaa038aecb49be8bc808d1b41fe395793
kri-pa08 <kriparani410@gmail.com>
2025-11-08 17:21:17 +0530

    Mediam Of Two Sorted Arrays

---
+++ Hard Problems/Median.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int[] merged = new int[n + m];
+
+        for (int i = 0; i < n; i++) {
+            merged[i] = nums1[i];
+        }
+
+        for (int i = 0; i < m; i++) {
+            merged[n + i] = nums2[i];
+        }
+
+        Arrays.sort(merged);
+
+        int total = n + m;
+        if (total % 2 == 0) {
+            return (merged[total/2 - 1] + merged[total/2]) / 2.0;
+        } else {
+            return merged[total/2];
+        }
+    }
+}
