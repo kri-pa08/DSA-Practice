a27c52726ec2a3afcd85aefa0b0ca88ae4683449
kri-pa08 <kriparani410@gmail.com>
2025-11-20 22:56:36 +0530

    First Unique Character in a string

--- MaxConsecutiveOne.java
+++ MaxConsecutiveOne.java
@@ -1,16 +1,16 @@
-class Solution {
+public class MaxConsecutiveOne {
+
     public int longestOnes(int[] nums, int k) {
-        int left = 0;  
-        int zeroCount = 0;  
-        int maxLen = 0;  
+        int left = 0;
+        int zeroCount = 0;
+        int maxLen = 0;
 
         for (int right = 0; right < nums.length; right++) {
-            
+
             if (nums[right] == 0) {
                 zeroCount++;
             }
 
-            
             while (zeroCount > k) {
                 if (nums[left] == 0) {
                     zeroCount--;
@@ -18,10 +18,18 @@ class Solution {
                 left++;
             }
 
-            
             maxLen = Math.max(maxLen, right - left + 1);
         }
 
         return maxLen;
+    }
+
+    public static void main(String[] args) {
+        MaxConsecutiveOne obj = new MaxConsecutiveOne();
+        
+        int[] nums = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1};
+        int k = 2;
+
+        System.out.println(obj.longestOnes(nums, k)); // Output: 6
     }
 }
