00901af7508171f7377f7c2c580211674ad43856
kri-pa08 <kriparani410@gmail.com>
2025-11-24 23:34:49 +0530

    First Unique Character

--- FirstUniqueCharacter.java
+++ FirstUniqueCharacter.java
@@ -1,15 +1,26 @@
-class Solution {
+class FirstUniqueCharacter {
     public int firstUniqChar(String s) {
         int[] freq = new int[26];
-    for (int i = 0; i<s.length(); i++){
-        freq[s.charAt(i) - 'a']++;
+
+        // Count frequency of each character
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        // Find first character with frequency 1
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
-    for (int i = 0; i<s.length(); i++){
-       if ( freq[s.charAt(i) - 'a'] ==1){
-            return i;
-        }
-    } 
-    return -1;
+    public static void main(String[] args) {
+        FirstUniqueCharacter sol = new FirstUniqueCharacter();
+        System.out.println(sol.firstUniqChar("leetcode")); // Output: 0
+        System.out.println(sol.firstUniqChar("loveleetcode")); // Output: 2
+        System.out.println(sol.firstUniqChar("aabb")); // Output: -1
     }
 }
