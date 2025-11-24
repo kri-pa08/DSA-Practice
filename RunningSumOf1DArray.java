883a4b158c6dc5fc80cfdf4340a1dd5c8dad587a
kri-pa08 <kriparani410@gmail.com>
2025-11-24 23:30:58 +0530

    Running Sum Of 1D Array

--- RunningSumOf1DArray.java
+++ RunningSumOf1DArray.java
@@ -1,13 +1,21 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] runningSum(int[] nums) {
-        
-        int[] result = new int[nums.length];  
+        int[] result = new int[nums.length];
         int sum = 0;
 
-        for(int i=0; i<nums.length; i++){
-           sum += nums[i];
-           result[i] = sum;
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            result[i] = sum;
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(sol.runningSum(nums))); // [1, 3, 6, 10]
     }
 }
