c862ea76df54b968d5a87fdee2a29e6dee2e7332
kri-pa08 <kriparani410@gmail.com>
2025-11-25 23:31:26 +0530

    Trapping Rain Water Leetcode

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -1,4 +1,5 @@
-class Solution {
+public class TrappingRainWater {
+
     public int trap(int[] height) {
         int left = 0;
         int right = height.length - 1;
@@ -7,6 +8,7 @@ class Solution {
         int waterTrapped = 0;
 
         while (left < right) {
+
             if (height[left] < height[right]) {
                 if (height[left] >= leftMax) {
                     leftMax = height[left];
@@ -24,5 +26,16 @@ class Solution {
             }
         }
         return waterTrapped;
+    }
+
+    // Test locally in VS Code
+    public static void main(String[] args) {
+        TrappingRainWater sol = new TrappingRainWater();
+
+        int[] arr1 = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(sol.trap(arr1));  // Output: 6
+
+        int[] arr2 = {4,2,0,3,2,5};
+        System.out.println(sol.trap(arr2));  // Output: 9
     }
 }
