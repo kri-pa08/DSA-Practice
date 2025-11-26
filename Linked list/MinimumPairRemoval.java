26683bd2219747c3ea5c678dd390af2078aa848a
kri-pa08 <kriparani410@gmail.com>
2025-11-26 23:55:51 +0530

    Minimum Pair Removal to sort array I leetcode 3507

--- Linked list/MinimumPairRemoval.java
+++ Linked list/MinimumPairRemoval.java
@@ -1,14 +1,30 @@
 class Solution {
     public int minimumPairRemoval(int[] nums) {
-        int[] freq = new int[100001];
-        for (int n : nums) freq[n]++;
-        
+        java.util.List<Integer> a = new java.util.ArrayList<>();
+        for (int n : nums) a.add(n);
         int ops = 0;
-        for (int f : freq) {
-            if (f == 1) return -1;
-            ops += f / 2;
+        while (hasInversion(a)) {
+            int minSum = Integer.MAX_VALUE, idx = -1;
+            for (int i = 0; i < a.size() - 1; i++) {
+                int s = a.get(i) + a.get(i + 1);
+                if (s < minSum) {
+                    minSum = s;
+                    idx = i;
+                }
+            }
+            a.set(idx, minSum);
+            a.remove(idx + 1);
+            ops++;
         }
         return ops;
     }
+
+    private boolean hasInversion(java.util.List<Integer> a) {
+        for (int i = 0; i < a.size() - 1; i++)
+            if (a.get(i) > a.get(i + 1))
+                return true;
+        return false;
+    }
 }
+
 //leetcode 3507
