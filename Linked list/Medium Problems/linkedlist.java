b2337d5ef7f8db47fd58c0a618f93e089b0c8161
kri-pa08 <kriparani410@gmail.com>
2025-12-08 23:53:44 +0530

    Merge two sorted list

--- Linked list/Medium Problems/linkedlist.java
+++ Linked list/Medium Problems/linkedlist.java
@@ -1,12 +1,53 @@
-class Solution {
-    public boolean hasCycle(ListNode head) {
-        ListNode slow = head, fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-            if (slow == fast) return true;
-        }
-        return false;
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
     }
 }
-//Linked List Cycle Leetcode
+
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+
+        if (list1 != null) temp.next = list1;
+        else temp.next = list2;
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+  
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        // Creating list2: 1 -> 3 -> 4
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        MergeTwoSortedLists obj = new MergeTwoSortedLists();
+        ListNode head = obj.mergeTwoLists(list1, list2);
+
+    
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+}
