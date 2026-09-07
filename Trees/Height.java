d93434564058ff404ee98a0760213be0a564b18f
Kripa <84517535+rishabhkumar786@users.noreply.github.com>
2026-09-07 16:20:35 +0530

    Height of Tree

---
+++ Trees/Height.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class TreeNode{
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+    public class Height{
+        public static void main(String[] args){
+            TreeNode root= new TreeNode(5);
+
+            root.left = new TreeNode(9);
+            root.right = new TreeNode(10);
+
+            root.left.left = new TreeNode(3);
+            root.left.right = new TreeNode(7);
+
+            root.right.left = new TreeNode(6);
+
+            int result = height(root);
+
+            System.out.println("Height = " + result);
+
+        }
+        static int height(TreeNode root){
+            if(root == null){
+                return 0;
+            }
+
+            int leftHeight = height(root.left);
+            int rightHeight = height(root.right);
+
+            return 1+Math.max(leftHeight, rightHeight);
+        }
+    }
