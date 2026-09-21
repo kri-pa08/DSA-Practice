534fad418b6d664a6fc1e796423ca6817f7b0a58
Kripa <kriparani410@gmail.com>
2026-09-21 23:45:37 +0530

    Bug Fixing

--- Hashing/TwoSum.java
+++ Hashing/TwoSum.java
@@ -1,10 +1,8 @@
 import java.util.*;
 public class TwoSum{
-    public static main (String[] args){
-        int[] arr = {1, 2, 3, 4};
-        int target = 5;
+     public static int[] twoSum(int[] arr, int target) {
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+    HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int i = 0; i<arr.length; i++){
             int current = arr[i];
@@ -16,8 +14,9 @@ public class TwoSum{
         }
         map.put(arr[i], i);
     }
-    
+    public static void main (String[] args){
+        int[] arr = {1, 2, 3, 4};
+        int target = 5;
     }
-
-    
-}
+    }
+}//edited
