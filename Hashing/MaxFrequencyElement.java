bd69707dcad927393af8820c3604dabe77d72a0b
Kripa <kriparani410@gmail.com>
2026-09-23 23:40:43 +0530

    Max Frequency

---
+++ Hashing/MaxFrequencyElement.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class MaxFrequencyElement{
+    public static void main(String[] args) {
+
+        int[] arr = {2, 2, 1, 1, 1, 2, 2};
+        for (int i = 0; i < arr.length; i++) {
+    int num = arr[i];
+
+    if (frequency.containsKey(num)) {
+        frequency.put(num, frequency.get(num) + 1);
+    } else {
+        frequency.put(num, 1);
+    }
+
+    if (frequency.get(num) > arr.length / 2) {
+        
+    }
+}
+}
+} //there is an error in this code
